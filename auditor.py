2a9de2f5bc961dadf3441a724f9715ec2c378a71
Andres Diaz <andresmigueldiazarismendi@gmail.com>
2025-12-01 20:56:36 -0400

    COAMBIOS MINISMOS

--- auditor.py
+++ auditor.py
@@ -23,23 +23,17 @@ def obtener_estado_actual(ruta):
 
 @registrar_accion
 def auditar_cambios(ruta_objetivo):
-    print(f"\n>>> DEBUG: Iniciando auditoría en '{ruta_objetivo}'")
     
     # 1. Cargar estado anterior
     estado_anterior = {}
     if os.path.exists(SNAPSHOT_FILE):
-        try:
+            
             with open(SNAPSHOT_FILE, 'r') as f:
                 estado_anterior = json.load(f)
-            print(f">>> DEBUG: Memoria cargada ({len(estado_anterior)} archivos recordados).")
-        except:
-            print(">>> DEBUG: Error leyendo memoria anterior.")
-    else:
-        print(">>> DEBUG: No hay memoria previa (Es la primera vez).")
 
     # 2. Obtener estado actual
     estado_actual = obtener_estado_actual(ruta_objetivo)
-    print(f">>> DEBUG: Archivos encontrados ahora mismo: {len(estado_actual)}")
+    
 
     # 3. Comparar
     set_anterior = set(estado_anterior.keys())
@@ -53,7 +47,6 @@ def auditar_cambios(ruta_objetivo):
         # Comparamos fechas (float)
         if estado_actual[archivo] != estado_anterior[archivo]:
             modificados.append(archivo)
-            print(f">>> DEBUG: Diferencia detectada en '{archivo}'")
             print(f"    Antes: {estado_anterior[archivo]} | Ahora: {estado_actual[archivo]}")
 
     # 4. Mostrar Resultados (FORCE PRINT)
@@ -76,4 +69,3 @@ def auditar_cambios(ruta_objetivo):
     # 5. Guardar
     with open(SNAPSHOT_FILE, 'w') as f:
         json.dump(estado_actual, f)
-    print("\n>>> DEBUG: Memoria actualizada. Si corres esto de nuevo inmediatamente, no saldrán cambios.")
